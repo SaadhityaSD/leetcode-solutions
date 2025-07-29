2ee81881163a376ad672a9310f1ca64393ca2ced
SaadhityaSD <sdsaadhitya13@gmail.com>
2025-07-29 20:38:24 +0530

    Time: 22 ms (35.74%) | Memory: 25.9 MB (46.15%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        a=set()
+        for i in nums:
+            if i in a:
+                return True
+            else:
+                a.add(i) 
+        return False           
+
+           
+                    
+
+        
+        
+        
